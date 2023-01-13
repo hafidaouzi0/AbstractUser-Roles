1380c810f61e74fe4e36104859d63909f3bf4e78
hafidaouzi0 <faouzibrahim2014@gmail.com>
2023-01-13 03:01:43 +0100

    again b

--- users/models.py
+++ users/models.py
@@ -1,21 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import (AbstractUser,BaseUserManager)
+from django.contrib.auth.models import AbstractUser,BaseUserManager
 # Create your models here.
 
 class User(AbstractUser):
-      class Role(models.TextChoices):
+      class role_user(models.TextChoices):
          ADMIN="ADMIN",'Admin'#'ADMIN': will be stored in the databse and 'Admin' will be readable human being
          STUDENT="STUDENT",'Student'
          TEACHER="TEACHER",'Teacher'
  
-      base_role=Role.ADMIN
+      base_role_user=role_user.ADMIN
 
-      Role=models.CharField(max_length=60,choices=Role.choices)
+      Role=models.CharField(max_length=60,choices=role_user.choices)
 
       def save(self,*args,**kwargs):
         #id the user doesn't have a pk : means if the user hasn't being created
         if not self.pk:
-            self.role=self.base_role
+            self.Role=self.base_role_user
         return super().save(*args,**kwargs)
 
 #A Manager is the interface through which database query operations are provided to Django models.
@@ -23,17 +23,22 @@ class User(AbstractUser):
 class StudentManager(BaseUserManager):
     def get_queryset(self,*args,**kwargs):
         results=super().get_queryset(*args,**kwargs)
-        return results.filter(role=User.Role.STUDENT)
+        return results.filter(Role=User.role_user.STUDENT)
 
 #a proxy model that inhirets from the Abstractuser won't get anpther table in the db 
 class Student(User):
-    base_role=User.Role.STUDENT
+
+    base_role_user= User.role_user.STUDENT
+
     student=StudentManager()
+    
     class Meta:
         proxy=True
     
     def welcome(self):
         return  "only for students"
+
+
 
 
 
@@ -43,7 +48,7 @@ class TeacherManager(BaseUserManager):
 
     def get_queryset(self,*args,**kwargs):
         results=super().get_queryset(*args,**kwargs)
-        return results.filter(role=User.Role.TEACHER)
+        return results.filter(Role=User.role_user.TEACHER)
 
 
 
@@ -52,7 +57,7 @@ class TeacherManager(BaseUserManager):
 
 class Teacher(User):
 
-    base_role=User.Role.TEACHER
+    base_role_user=User.role_user.TEACHER
     teacher=TeacherManager()
 
     class Meta:
